143ec27ec7e55a60d9a4e18b9e3f4d6002bf6ece
unknown <wpkszan@gmail.com>
2021-10-19 01:54:44 -0300

    remoção imgs de fundo

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,8 +1,5 @@
 import { StyledDiv, StyledButton } from "./styles";
 import { Link } from "react-scroll";
-import bispo from "../../assets/chess/bispo.png"
-import dama from "../../assets/chess/dama.png"
-import cavalo from "../../assets/chess/cavalo.png"
 import { useLanguage } from "../../hooks/useLanguage";
 
 export function Home() {
@@ -23,11 +20,6 @@ export function Home() {
     }else{
         return( 
             <StyledDiv id="home">
-    
-    
-                <img className="bispo" src={bispo} alt="bispo" />
-                <img className="cavalo" src={cavalo} alt="dama" />
-                <img className="dama" src={dama} alt="dama" />
     
                 <div>
                     <h2>William Peterson Kszan<span>.</span></h2>
