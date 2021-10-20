171ec0a646d449c5c0c2204f464178fd28b629cf
unknown <wpkszan@gmail.com>
2021-10-19 22:28:39 -0300

    trying to solve netfly bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,46 +8,29 @@ import { useState } from "react/cjs/react.development";
 
 export function Header() {
     
-    const [openMenu] = useState("isClosed")
+    const [openMenu, setOpenMenu] = useState("isClosed")
     
 
+    const handleIsOpen = function(){
+        if(openMenu !== "isOpen"){
+            setOpenMenu("isOpen")
+        }else{
+            setOpenMenu("isClosed")
+        }
+    }
 
 
     const lang = useLanguage();
 
     return(
-        
-
-        //Componente com nav utilizando react-scroll para animação e navegação entre os componentes da pagina.
         <StyledHeader>
-            
-            <nav className="menu">
-                <Hamburger>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </Hamburger>
-                <NavLinks className={openMenu}>
-                    <li><Link  to="home" smooth={true} duration={1000}>Home</Link></li>
-                    <li><Link  to="sobre" smooth={true} duration={1000}>Sobre</Link></li>
-                    <li><Link  to="oqfaco" smooth={true} duration={1000}>Skills</Link></li>
-                    <li><Link  to="portfolio" smooth={true} duration={1000}>Portfolio</Link></li>
-                    <li><Link  to="contato" smooth={true} duration={1000}>Contato</Link></li>
-                </NavLinks>  
-            </nav>
-
-        
-            <nav className="lingua">
-                <div className={lang.ptbr}>
-                    <img src={ptbr} alt="bandeira-brasil"/>
-                </div>
-                <div className={lang.en}>
-                    <img src={en} alt="bandeira-ingraterra"/>
-                </div>
-                
-            </nav>
-
+            <Hamburger>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </Hamburger>
         </StyledHeader>
+
     )
 
     
