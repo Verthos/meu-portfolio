48821d154e16631afaf5059c130b12a7401d1182
unknown <wpkszan@gmail.com>
2021-10-19 21:41:03 -0300

    trying to solve netfly bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
-import { Hamburger, NavLinks, StyledHeader } from "./styles"
-import { Link } from "react-scroll";
-import ptbr from "../../assets/pt-br.jpg"
-import en from "../../assets/en.jpg"
+//import { Hamburger, NavLinks, StyledHeader } from "./styles"
+//import { Link } from "react-scroll";
+//import ptbr from "../../assets/pt-br.jpg"
+//import en from "../../assets/en.jpg"
 import { useLanguage } from "../../hooks/useLanguage";
 import { useState } from "react/cjs/react.development";
 
