d8b130d2c45f98c5a45292c4c6f86f54205215e4
unknown <wpkszan@gmail.com>
2021-10-19 22:01:30 -0300

    trying to solve netfly bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,23 @@
-import { StyledHeader } from "./styles"
+import { Hamburger, NavLinks, StyledHeader } from "./styles"
+import { Link } from "react-scroll";
 import ptbr from "../../assets/pt-br.jpg"
 import en from "../../assets/en.jpg"
 import { useLanguage } from "../../hooks/useLanguage";
-
+import { useState } from "react/cjs/react.development";
 
 
 export function Header() {
     
-   
+    const [openMenu, setOpenMenu] = useState("isClosed")
     
 
-    
+    const handleIsOpen = function(){
+        if(openMenu !== "isOpen"){
+            setOpenMenu("isOpen")
+        }else{
+            setOpenMenu("isClosed")
+        }
+    }
 
 
     const lang = useLanguage();
@@ -20,9 +27,20 @@ export function Header() {
 
         //Componente com nav utilizando react-scroll para animação e navegação entre os componentes da pagina.
         <StyledHeader>
-            /
+            
             <nav className="menu">
-                
+                <Hamburger onClick={() => handleIsOpen()}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </Hamburger>
+                <NavLinks className={openMenu}>
+                    <li><Link onClick={() => handleIsOpen()} to="home" smooth={true} duration={1000}>Home</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="sobre" smooth={true} duration={1000}>Sobre</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="oqfaco" smooth={true} duration={1000}>Skills</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="portfolio" smooth={true} duration={1000}>Portfolio</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="contato" smooth={true} duration={1000}>Contato</Link></li>
+                </NavLinks>  
             </nav>
 
         
