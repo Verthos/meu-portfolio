c45f16a8e0dd6b7e0dedf5c2500aff40836a58ea
unknown <wpkszan@gmail.com>
2021-10-19 21:49:50 -0300

    trying to solve netfly bug

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -98,8 +98,6 @@ export const NavLinks = styled.ul`
         height: 100vh;
         width: 100%;
         flex-direction: column;
-        clip-path: circle(100px at 90% -10%);
-        -webkit-clip-path: circle(100px at 5% -10%);
         transition: all 1s ease-out;
         pointer-events: none;
     
@@ -112,10 +110,8 @@ export const NavLinks = styled.ul`
             }
 
         &.isOpen{
-            clip-path: circle(1000px at 90% -10%);
-            -webkit-clip-path: circle(1000px at 5% -10%);
+
             pointer-events: all
-            
         }
 
       
