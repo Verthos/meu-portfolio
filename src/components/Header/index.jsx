52f1ca3775640f11c6834e9d0f400432a837ba46
unknown <wpkszan@gmail.com>
2021-10-19 22:21:35 -0300

    trying to solve netfly bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,23 @@
-import { StyledHeader } from "./styles"
+import { Hamburger, NavLinks, StyledHeader } from "./styles"
+import { Link } from "react-scroll";
 import ptbr from "../../assets/pt-br.jpg"
 import en from "../../assets/en.jpg"
 import { useLanguage } from "../../hooks/useLanguage";
+import { useState } from "react/cjs/react.development";
 
 
 export function Header() {
+    
+    const [openMenu, setOpenMenu] = useState("isClosed")
+    
 
+    const handleIsOpen = function(){
+        if(openMenu !== "isOpen"){
+            setOpenMenu("isOpen")
+        }else{
+            setOpenMenu("isClosed")
+        }
+    }
 
 
     const lang = useLanguage();
@@ -16,15 +28,28 @@ export function Header() {
         //Componente com nav utilizando react-scroll para animação e navegação entre os componentes da pagina.
         <StyledHeader>
             
-            
+            <nav className="menu">
+                <Hamburger onClick={() => handleIsOpen()}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </Hamburger>
+                <NavLinks className={openMenu}>
+                    <li><Link onClick={() => handleIsOpen()} to="home" smooth={true} duration={1000}>Home</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="sobre" smooth={true} duration={1000}>Sobre</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="oqfaco" smooth={true} duration={1000}>Skills</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="portfolio" smooth={true} duration={1000}>Portfolio</Link></li>
+                    <li><Link onClick={() => handleIsOpen()} to="contato" smooth={true} duration={1000}>Contato</Link></li>
+                </NavLinks>  
+            </nav>
 
         
             <nav className="lingua">
                 <div className={lang.ptbr}>
-                    <img src={ptbr} alt="bandeira-brasil" />
+                    <img src={ptbr} alt="bandeira-brasil"/>
                 </div>
                 <div className={lang.en}>
-                    <img src={en} alt="bandeira-ingraterra" />
+                    <img src={en} alt="bandeira-ingraterra"/>
                 </div>
                 
             </nav>
