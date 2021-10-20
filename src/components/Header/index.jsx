016eeaaaac8129be8d1c19adfbc14ea476a7aa03
unknown <wpkszan@gmail.com>
2021-10-19 23:49:26 -0300

    trying to solve netfly bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,37 @@
-import { Hamburger} from "./styles"
-import { Link } from "react-scroll"
-import { useLanguage } from "../../hooks/useLanguage";
-import en from "../../assets/en.jpg"
-import ptbr from "../../assets/pt-br.jpg"
+import {  NavLinks, StyledHeader } from "./styles"
+import { Link } from "react-scroll";
 
 
 export function Header() {
+    
+    //const [openMenu] = useState("isClosed")
+    
 
-    const lang = useLanguage();
+    //const handleIsOpen = function(){
+        //if(openMenu !== "isOpen"){
+          //  setOpenMenu("isOpen")
+        //}else{
+        //    setOpenMenu("isClosed")
+      //  }
+    //}
+
+
 
     return(
-        <header>
-            <Hamburger >
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
-            </Hamburger>
-            <ul>
-                <Link to="contato" smooth={true} duration={1000}>Entre em contato</Link>
-                <Link to="contato" smooth={true} duration={1000}>Entre em contato</Link>
-                <Link to="contato" smooth={true} duration={1000}>Entre em contato</Link>
-                <Link to="contato" smooth={true} duration={1000}>Entre em contato</Link>
-                <Link to="contato" smooth={true} duration={1000}>Entre em contato</Link>
-            </ul>
-            
-            <button onClick={lang.handleNewLanguage("ptbr")}><img src={ptbr} alt="bandeira brasil"/></button>
-            <button onClick={lang.handleNewLanguage("en")}><img src={en} alt="bandeira reino unido"/></button>
-        </header>
 
+        //Componente com nav utilizando react-scroll para animação e navegação entre os componentes da pagina.
+        <StyledHeader>
+              
+            <nav className="menu">
+                <NavLinks>
+                    <li><Link to="home" smooth={true} duration={1000}>Home</Link></li>
+                    <li><Link to="sobre" smooth={true} duration={1000}>Sobre</Link></li>
+                    <li><Link to="oqfaco" smooth={true} duration={1000}>Skills</Link></li>
+                    <li><Link to="portfolio" smooth={true} duration={1000}>Portfolio</Link></li>
+                    <li><Link to="contato" smooth={true} duration={1000}>Contato</Link></li>
+                </NavLinks>  
+            </nav>
+
+        </StyledHeader>
     )
-
-    
 }
