bb3a663b9d19317e96f7300b3583f9dd3e4a3172
unknown <wpkszan@gmail.com>
2021-10-19 22:55:38 -0300

    trying to solve netfly bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,17 @@
 import { Hamburger, StyledHeader, NavLinks } from "./styles"
 import { Link } from "react-scroll"
 //import { useLanguage } from "../../hooks/useLanguage";
-import { useState } from "react/cjs/react.development";
 
 
 export function Header() {
-    
-    const [openMenu, setOpenMenu] = useState("isClosed")
-    
 
-    const handleIsOpen = function(arg){
-        if(openMenu !== arg){
-            setOpenMenu("isOpen")
-        }else{
-            setOpenMenu("isClosed")
-        }
-        console.log(openMenu)
-    }
 
 
     //const lang = useLanguage();
 
     return(
         <StyledHeader>
-            <Hamburger onClick={() => handleIsOpen("isOpen")}>
+            <Hamburger >
                 <div className="line"></div>
                 <div className="line"></div>
                 <div className="line"></div>
