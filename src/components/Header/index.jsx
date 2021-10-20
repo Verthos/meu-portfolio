3a3dddbb6b7739c19757bad1e2e647556e353049
unknown <wpkszan@gmail.com>
2021-10-19 21:55:15 -0300

    trying to solve netfly bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -29,18 +29,7 @@ export function Header() {
         <StyledHeader>
             /
             <nav className="menu">
-                <Hamburger onClick={() => handleIsOpen()}>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </Hamburger>
-                <NavLinks className={openMenu}>
-                    <li><Link onClick={() => handleIsOpen()} to="home" smooth={true} duration={1000}>Home</Link></li>
-                    <li><Link onClick={() => handleIsOpen()} to="sobre" smooth={true} duration={1000}>Sobre</Link></li>
-                    <li><Link onClick={() => handleIsOpen()} to="oqfaco" smooth={true} duration={1000}>Skills</Link></li>
-                    <li><Link onClick={() => handleIsOpen()} to="portfolio" smooth={true} duration={1000}>Portfolio</Link></li>
-                    <li><Link onClick={() => handleIsOpen()} to="contato" smooth={true} duration={1000}>Contato</Link></li>
-                </NavLinks>  
+                
             </nav>
 
         
